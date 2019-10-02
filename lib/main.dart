5c85b75d4cb094bc986692070dda42ad94b6a67f
Railton98 <railtonleal98@gmail.com>
2019-10-02 02:32:32 -0300

    creating mobile template

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'SON Contador de Cursos',
       home: new HomeApp(),
     );
@@ -22,26 +23,46 @@ class HomeApp extends StatefulWidget {
 class _HomeAppState extends State<HomeApp> {
   @override
   Widget build(BuildContext context) {
-    return new Container(
-      color: Color.fromARGB(255, 0, 0, 0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Text(
-            "Hello World, Flutter!",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.blueAccent, decoration: TextDecoration.none),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("School of Net"),
+        backgroundColor: Colors.lightGreen,
+      ),
+      body: new Container(
+        // color: Color.fromARGB(255, 0, 0, 0),
+        constraints: BoxConstraints.expand(),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Hello World, Flutter!",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.blueAccent, decoration: TextDecoration.none),
+            ),
+            Text(
+              "animado com dart!",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.purpleAccent,
+                  fontSize: 35,
+                  decoration: TextDecoration.none),
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("home"),
           ),
-          Text(
-            "animado com dart!",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-                color: Colors.purpleAccent,
-                fontSize: 35,
-                decoration: TextDecoration.none),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            title: Text("School"),
           ),
         ],
+        fixedColor: Colors.lightGreenAccent,
       ),
     );
   }
